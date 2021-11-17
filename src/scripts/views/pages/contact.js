f831e29baf9dd4f7c744fdbdde4375ae4f096386
admajarois <rois.admj@gmail.com>
2021-11-17 12:15:47 +0700

    edit hompage hero gradient background overlay

--- src/scripts/views/pages/contact.js
+++ src/scripts/views/pages/contact.js
@@ -6,16 +6,16 @@ const Contact = {
                     <h1 class="text-center text-white fw-bold">Contact Me</h1>
                 </div>
             </div>
-            <div class="container my-4">
-                <div class="row align-items-md-stretch">
-                    <div class="col-md-6">
+            <div class="container contact-section my-4">
+                <div class="row contact-section-child align-items-md-stretch">
+                    <div class="col-md-8">
                         <div class="h-100 p-5 text-white bg-dark rounded-3">
                             <h2>Change the background</h2>
                             <p>Swap the background-color utility and add a color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
                             <button class="btn btn-outline-light" type="button">Example button</button>
                         </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <div class="h-100 p-5 bg-light border rounded-3">
                             <h2>Add borders</h2>
                             <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
