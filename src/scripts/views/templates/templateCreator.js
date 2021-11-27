1c128310b00fcc3333b81eeac552ea72313ec40e
admajarois <rois.admj@gmail.com>
2021-11-27 14:50:19 +0700

    add some code

--- src/scripts/views/templates/templateCreator.js
+++ src/scripts/views/templates/templateCreator.js
@@ -1,5 +1,3 @@
-import icons from '../../globals/skills';
-
 const skillTemplate=(icon)=> `
     <div class="col p-3 rounded-3 d-flex justify-content-center">
         <img src="${icon.icon}" class="img-thumbnail" alt="icons skills"/>
